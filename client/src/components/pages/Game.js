84ebc2fe2cd368f241391f36171531361f7364ad
mielu22 <bupton18@mit.edu>
2021-01-12 21:56:39 -0500

    uh this Game.js

---
+++ client/src/components/pages/Game.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Game extends Component {
+  constructor(props){
+    super(props);
+    this.state = {}; //initialize state
+  }
+
+  componentDidMount(){
+    // for api calls
+  }
+
+  render() {
+    return(
+      <div> What's up, GAMERS </div>
+    );
+  }
+}
+
+export default Game;
