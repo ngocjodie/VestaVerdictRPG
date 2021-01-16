664cf7912538be1093f475dfcde7565387f571a5
ngocjodie <ngoc.jodie@gmail.com>
2021-01-16 16:42:56 -0600

    testing out database

--- server/server.js
+++ server/server.js
@@ -33,11 +33,11 @@ const socketManager = require("./server-socket");
 
 // Server configuration below
 // TODO change connection URL after setting up your team database
-const mongoConnectionURL = "FILL ME IN";
+const mongoConnectionURL = "mongodb+srv://jodie:e6O6uo1dhQFje5pf@cluster0.0loe4.mongodb.net/Cluster0?retryWrites=true&w=majority";
 //process.env.mongoURL;
 
 // TODO change database name to the name you chose
-const databaseName = "FILL ME IN";
+const databaseName = "Cluster0";
 
 // connect to mongodb
 mongoose
