d9f2e4aeeabcff1fe5e721d5c0f916f96442d687
mielu22 <bupton18@mit.edu>
2021-01-17 22:55:43 -0500

    purged screen's arrow pad

--- client/src/components/modules/dummy.js
+++ client/src/components/modules/dummy.js
@@ -60,19 +60,9 @@ const directions = {
    "ArrowLeft": "left",
    "ArrowRight": "right",
 }
-/*
-const keys = {
-   38: directions.up,
-   37: directions.left,
-   39: directions.right,
-   40: directions.down,
-}
-*/
 
 document.addEventListener("keydown", (e) => {
-  console.log("got an arrow key"); 
   const dir = directions[e.key];
-  console.log("and registered", dir);
    if (dir && held_directions.indexOf(dir) === -1) {
       held_directions.unshift(dir)
    }
@@ -88,7 +78,7 @@ document.addEventListener("keyup", (e) => {
 
 
 
-/* BONUS! Dpad functionality for mouse and touch */
+/* Was dpad stuff, but might be useful for clickable things
 let isPressed = false;
 const removePressedAll = () => {
    document.querySelectorAll(".dpad-button").forEach(d => {
@@ -104,30 +94,4 @@ document.body.addEventListener("mouseup", () => {
    isPressed = false;
    held_directions = [];
    removePressedAll();
-})
-const handleDpadPress = (direction, click) => {   
-   if (click) {
-      isPressed = true;
-   }
-   held_directions = (isPressed) ? [direction] : []
-   
-   if (isPressed) {
-      removePressedAll();
-      document.querySelector(".dpad-"+direction).classList.add("pressed");
-   }
-}
-//Bind a ton of events for the dpad
-document.querySelector(".dpad-left").addEventListener("touchstart", (e) => handleDpadPress(directions.left, true));
-document.querySelector(".dpad-up").addEventListener("touchstart", (e) => handleDpadPress(directions.up, true));
-document.querySelector(".dpad-right").addEventListener("touchstart", (e) => handleDpadPress(directions.right, true));
-document.querySelector(".dpad-down").addEventListener("touchstart", (e) => handleDpadPress(directions.down, true));
-
-document.querySelector(".dpad-left").addEventListener("mousedown", (e) => handleDpadPress(directions.left, true));
-document.querySelector(".dpad-up").addEventListener("mousedown", (e) => handleDpadPress(directions.up, true));
-document.querySelector(".dpad-right").addEventListener("mousedown", (e) => handleDpadPress(directions.right, true));
-document.querySelector(".dpad-down").addEventListener("mousedown", (e) => handleDpadPress(directions.down, true));
-
-document.querySelector(".dpad-left").addEventListener("mouseover", (e) => handleDpadPress(directions.left));
-document.querySelector(".dpad-up").addEventListener("mouseover", (e) => handleDpadPress(directions.up));
-document.querySelector(".dpad-right").addEventListener("mouseover", (e) => handleDpadPress(directions.right));
-document.querySelector(".dpad-down").addEventListener("mouseover", (e) => handleDpadPress(directions.down));
+})  */
