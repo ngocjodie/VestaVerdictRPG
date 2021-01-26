8c5538404b8b43f34501618c915ff47c0ddfcbe9
Virginia Rosenberger <vrosenb@mit.edu>
2021-01-26 00:47:28 -0500

    profile page lookin swanky

--- client/src/components/pages/Frontpage.js
+++ client/src/components/pages/Frontpage.js
@@ -8,8 +8,10 @@ import Rhea from "./Rheas.png"
 class Frontpage extends Component{
     constructor(props){
       super(props);
-    }
-  
+      this.state = {
+        hasWon: false,
+    };
+}
     componentDidMount() {
       console.log(this.props.userId)
     }
@@ -17,10 +19,33 @@ class Frontpage extends Component{
 render() {
     return (
       <>
+      
     <img src={Rhea} className="Profile-pic"/>
+    <div className="Profile-name"> RHEA</div>
+    <br></br>
+    <div class="fading_line"></div>
+    <div className="Profile-info"> AWARDS CABINET </div>
+    <div class="container">
+  <div class="holder h1">
+  <div class="tooltip"> 
+  <span class="tooltiptext">Tooltip text</span>
+</div>
+  </div>
+  <div className={this.state.hasWon ? "holder h1":"holder h3"}></div>
+  <div class="holder h3"></div>
+  <div class="holder h4"></div>
+</div>
+<div class="container">
+  <div class="holder h1"></div>
+  <div class="holder h2"></div>
+  <div class="holder h3"></div>
+  <div class="holder h4"></div>
+</div>
 
 
-    <Playthroughs userId={this.props.userId}/>
+
+  
+<Playthroughs userId={this.props.userId}/>
 
     <Awards userId={this.props.userId}/>
     </>
